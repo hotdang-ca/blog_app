e258a7b8c85b7422ef95ee1de42714d49820fdf8
James Perih <james@hotdang.ca>
2022-10-08 13:42:56 -0600

    Small change to set the app-wide font theme.

--- lib/src/blog_app.dart
+++ lib/src/blog_app.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/src/router/route_config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BlogApp extends StatelessWidget {
   const BlogApp({Key? key}) : super(key: key);
@@ -9,6 +10,9 @@ class BlogApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: RouteConfig.routeConfig,
       title: 'GoRouter Example',
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
     );
   }
 }
