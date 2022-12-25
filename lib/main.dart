becda16491c44f5eb08e434b3a352054c4511587
James Perih <james@hotdang.ca>
2022-12-25 10:50:08 -0600

    Change to Path URL strategy.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:blog_app/src/blog_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const BlogApp());
 }
